c185e5fb5dc7c2e448eff9f457cf791adeae872d
mzmarci <mzmazy100@gmail.com>
2023-08-21 05:29:46 +0100

    updating

--- prometheus-installer.sh
+++ prometheus-installer.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 #This is to inforce supplying download location
 if [ $# -lt 1 ]
 then
@@ -7,18 +8,19 @@ then
   exit 1
 fi
 
+
 # this is setting important variables
 download_endpoint=$1
 install_location="${2:-/home/ec2-user/prom/}"
 run_prom_after_install="${3:-yes}"
 
-echo download_endpoint is "[ $download_endpoint ]"
-echo install_location is "[ $install_location ]"
-echo run_prom_after_install is "[ $run_prom_after_install]"
+echo "download_endpoint is [ $download_endpoint ]"
+echo "install_location is [ $install_location ]"
+echo "run_prom_after_install is [ $run_prom_after_install]"
 
 #create and change to given directory
 echo "create and changing to $install_location"
-mkdir -p "$install_location
+mkdir p "$install_location"
 cd "$install_location"
 
 #download binary
@@ -40,6 +42,3 @@ then
 else
   echo "prometheus installed but not started"
 fi
-
-
-
